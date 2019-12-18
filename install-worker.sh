12d172dfee260c46c159ebe65509e7e90b1b4712
jackalstar88 <jackalstar88@gmail.com>
2019-12-18 09:32:11 -0500

    Fix position of sonobuoy e2e registry config check (#590)

--- install-worker.sh
+++ install-worker.sh
@@ -204,6 +204,11 @@ sudo mv $TEMPLATE_DIR/eni-max-pods.txt /etc/eks/eni-max-pods.txt
 sudo mv $TEMPLATE_DIR/bootstrap.sh /etc/eks/bootstrap.sh
 sudo chmod +x /etc/eks/bootstrap.sh
 
+if [[ -n "$SONOBUOY_E2E_REGISTRY" ]]; then
+    sudo mv $TEMPLATE_DIR/sonobuoy-e2e-registry-config /etc/eks/sonobuoy-e2e-registry-config
+    sed -i s,SONOBUOY_E2E_REGISTRY,$SONOBUOY_E2E_REGISTRY,g /etc/eks/sonobuoy-e2e-registry-config
+fi
+
 ################################################################################
 ### AMI Metadata ###############################################################
 ################################################################################
